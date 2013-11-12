146c835d06ac98d2d880c43669deb861b76b2265
vingo Chen <vingosheng@gmail.com>
2013-11-12 15:05:43 -0800

    complete game loop

---
+++ SpaceInvader/GameOverLayer.h
@@ -0,0 +1,13 @@
+//
+//  GameOverLayer.h
+//  SpaceInvader
+//
+//  Created by vingosj on 13-11-10.
+//  Copyright (c) 2013年 vingosj. All rights reserved.
+//
+
+#import "CCLayer.h"
+
+@interface GameOverLayer : CCLayerColor
+
+@end
---
+++ SpaceInvader/GameOverLayer.m
@@ -0,0 +1,13 @@
+//
+//  GameOverLayer.m
+//  SpaceInvader
+//
+//  Created by vingosj on 13-11-10.
+//  Copyright (c) 2013年 vingosj. All rights reserved.
+//
+
+#import "GameOverLayer.h"
+
+@implementation GameOverLayer
+
+@end
